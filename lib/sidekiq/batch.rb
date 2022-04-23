9f518c34670a550b5591aa8c44086f994a115e0b
Liubomyr Manastyretskyi <manastyretskyi@gmail.com>
2022-04-23 15:25:21 +0300

    Fix batch valid?

--- lib/sidekiq/batch.rb
+++ lib/sidekiq/batch.rb
@@ -130,7 +130,7 @@ module Sidekiq
     end
 
     def valid?(batch = self)
-      valid = !Sidekiq.redis { |r| r.exists("invalidated-bid-#{batch.bid}") }
+      valid = Sidekiq.redis { |r| r.exists("invalidated-bid-#{batch.bid}") } == 0
       batch.parent ? valid && valid?(batch.parent) : valid
     end
 
